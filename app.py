251bef436264c9a6e46f98699cb73ab03764129b
Saleh Eddine Touil <168127706+saleheddinetouil@users.noreply.github.com>
2025-02-01 13:02:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException, TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,6 +15,7 @@ import base64
 from pathlib import Path
 import stat
 import tempfile
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -51,25 +51,12 @@ def setup_driver():
         options.add_argument('--disable-dev-shm-usage')
         prefs = {"profile.managed_default_content_settings.images": 2}
         options.add_experimental_option("prefs", prefs)
+      
         
-        
-        
-        driver_path =  Path(tempfile.gettempdir()) / "chromedriver"
-        # Set execution permissions to the chromedriver
-        if os.path.exists(str(driver_path)):
-             driver_path.chmod(driver_path.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-        else:
-            logging.info("ChromeDriver is not available, downloading..")
-            
-            driver_path_download = ChromeDriverManager().install()
-            driver_path = Path(driver_path_download)
-            
-            driver_path.chmod(driver_path.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-            
-            
-        service = Service(executable_path=str(driver_path))
+        # Use the pre-installed chromedriver path
+        service = Service(executable_path="/usr/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=options)
-        logging.info("ChromeDriver setup successfully.")
+        logging.info("ChromeDriver setup successfully using pre-installed driver.")
         return driver
     except WebDriverException as e:
         logging.error(f"Error setting up ChromeDriver: {e}")
@@ -391,7 +378,6 @@ def main():
            if driver:
               driver.quit()
               logging.info("ChromeDriver closed.")
-           
 
 if __name__ == "__main__":
     main()
